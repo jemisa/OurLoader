0996403ebad27b4181a858b7f2ebf18f40bf8c90
coc4tm <corentin.bzh@outlook.com>
2016-09-11 09:24:14 +0400

    Added files from computer

---
+++ src/main.c
@@ -0,0 +1,105 @@
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include "dynamic_libs/os_functions.h"
+#include "dynamic_libs/fs_functions.h"
+#include "dynamic_libs/gx2_functions.h"
+#include "dynamic_libs/sys_functions.h"
+#include "dynamic_libs/vpad_functions.h"
+#include "dynamic_libs/padscore_functions.h"
+#include "dynamic_libs/socket_functions.h"
+#include "dynamic_libs/ax_functions.h"
+#include "fs/fs_utils.h"
+#include "fs/sd_fat_devoptab.h"
+#include "system/memory.h"
+#include "utils/logger.h"
+#include "utils/utils.h"
+#include "common/common.h"
+
+/* Entry point */
+int Menu_Main(void)
+{
+    //!*******************************************************************
+    //!                   Initialize function pointers                   *
+    //!*******************************************************************
+    //! do OS (for acquire) and sockets first so we got logging
+    InitOSFunctionPointers();
+    InitSocketFunctionPointers();
+
+    log_init("192.168.178.3");
+    log_print("Starting launcher\n");
+
+    InitFSFunctionPointers();
+    InitVPadFunctionPointers();
+
+    log_print("Function exports loaded\n");
+
+    //!*******************************************************************
+    //!                    Initialize heap memory                        *
+    //!*******************************************************************
+    log_print("Initialize memory management\n");
+    //! We don't need bucket and MEM1 memory so no need to initialize
+    memoryInitialize();
+
+    //!*******************************************************************
+    //!                        Initialize FS                             *
+    //!*******************************************************************
+    log_printf("Mount SD partition\n");
+    mount_sd_fat("sd");
+
+    VPADInit();
+
+    // Prepare screen
+    int screen_buf0_size = 0;
+    int screen_buf1_size = 0;
+
+    // Init screen and screen buffers
+    OSScreenInit();
+    screen_buf0_size = OSScreenGetBufferSizeEx(0);
+    screen_buf1_size = OSScreenGetBufferSizeEx(1);
+
+    unsigned char *screenBuffer = MEM1_alloc(screen_buf0_size + screen_buf1_size, 0x40);
+
+    OSScreenSetBufferEx(0, screenBuffer);
+    OSScreenSetBufferEx(1, (screenBuffer + screen_buf0_size));
+
+    OSScreenEnableEx(0, 1);
+    OSScreenEnableEx(1, 1);
+
+    // Clear screens
+    OSScreenClearBufferEx(0, 0);
+    OSScreenClearBufferEx(1, 0);
+
+   //Text printing ?
+
+    // Flush the cache
+    DCFlushRange(screenBuffer, screen_buf0_size);
+    DCFlushRange((screenBuffer + screen_buf0_size), screen_buf1_size);
+
+    // Flip buffers
+    OSScreenFlipBuffersEx(0);
+    OSScreenFlipBuffersEx(1);
+
+	MEM1_free(screenBuffer);
+	screenBuffer = NULL;
+
+   
+        char buf_vol_odd[20];
+        snprintf(buf_vol_odd, sizeof(buf_vol_odd), "%s", "/vol/storage_odd03");
+        _SYSLaunchTitleByPathFromLauncher(buf_vol_odd, 18, 0);
+
+    //!*******************************************************************
+    //!                    Enter main application                        *
+    //!*******************************************************************
+
+    log_printf("Unmount SD\n");
+    unmount_sd_fat("sd");
+    log_printf("Release memory\n");
+    memoryRelease();
+    log_deinit();
+
+    return EXIT_SUCCESS;
+}
+
